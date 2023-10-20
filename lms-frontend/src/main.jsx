9595260419eac6caa58ddbfd76b2db584b730ebe
Tapas Pradhan <pradhanta20@gmail.com>
2023-10-20 20:28:23 +0530

    Today i work on authslice

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -1,16 +1,20 @@
-// Components Import
+
 import App from './App.jsx'
-// CSS Import
+
 import './index.css'
-// Libriary Import
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
+import store from './Redux/store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <Toaster/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
+  </Provider>
 )
